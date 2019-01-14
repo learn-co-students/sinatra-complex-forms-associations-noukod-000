103c4fc4b655c3f8c75d880d2b5d808ac5fae354
Samuel Cornet <samuel@noukod.com>
2019-01-14 15:14:41 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190112160229) do
+ActiveRecord::Schema.define(version: 2) do
 
   create_table "owners", force: :cascade do |t|
     t.string "name"
